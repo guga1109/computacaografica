97f6c821c163457a46a38e619037d4c3229babc5
guga1109 <36571491+guga1109@users.noreply.github.com>
2020-11-30 21:13:29 -0300

    Trabalho 5 fazenda

--- Trabalho5/js/script.js
+++ Trabalho5/js/script.js
@@ -1,125 +1,190 @@
-
 var scene;
 var camera;
 var renderer;
+var ground;
+var objLoader;
+var textureLoader;
+var spotLight;
+var obj; //objeto dinamico.
 
-var criaGround = function(){
-    textureLoader = new THREE.TextureLoader();
-    groundTexture = textureLoader.load('assets/grass.jpg');
-    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-    groundTexture.repeat.set(250, 250);
-    groundTexture.anisotropy = 16;
-    groundTexture.encoding = THREE.sRGBEncoding;
+var cavalo = [];
+var pato = [];
+var porco = [];
+var pinguin = [];
+var elefante = [];
 
-    ground = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry(100, 100, 10),
-        new THREE.MeshBasicMaterial({map: groundTexture})
-    );
+var guiFunction = function(){
+	const gui = new dat.GUI();
+	param = {
+		animal: ""
+	};    
+	var chAnimal = gui.add(param, 'animal', ['cavalo', 'pato', 'porco', 'pinguin', 'elefante']).name("Animal");
+	chAnimal.onChange(function(parametroQualquer){
+		if (parametroQualquer == 'cavalo'){
+			console.log(cavalo.position);
+			camera.lookAt(cavalo.position);
+		} else if (parametroQualquer == 'porco'){
+			console.log(porco.position);
+			camera.lookAt(porco.position);
+		} else if (parametroQualquer == 'porco'){
+			console.log(pinguin.position);
+			camera.lookAt(pinguin.position);
+		} else if (parametroQualquer == 'elefante'){
+			console.log(elefante.position);
+			camera.lookAt(elefante.position);
+		} else if (parametroQualquer == 'pato'){
+			console.log(pato.position);
+			camera.lookAt(pato.position);
+		}
+	});
+	gui.open();
+};
 
-    ground.rotation.x -= Math.PI / 2;
-    ground.position.y =-2;
+var criaGround = function (){
+	textureLoader = new THREE.TextureLoader();
+	groundTexture = textureLoader.load('assets/grass.jpg');
+	groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+	groundTexture.repeat.set(20, 20);
+	groundTexture.anisotropy = 16;
+	groundTexture.encoding = THREE.sRGBEncoding;
+	ground = new  THREE.Mesh(
+		new THREE.PlaneGeometry(1050, 1050, 25,25),
+		new THREE.MeshBasicMaterial({map : groundTexture})
+	);
+	ground.rotation.x -= Math.PI / 2;
+	ground.position.y=-2;
+	scene.add(ground);
+};
 
-    scene.add(ground);
-}
-
-var loadObj = function(assetName, scale, rotationX, rotationY, rotationZ){
-    objLoader = new THREE.OBJLoader();
-
-    objLoader.load(
-        'assets/' + assetName + ".obj",
-        function(object){
-            objCarregado = object;
-            objCarregado.scale.x = scale;
-            objCarregado.scale.y = scale;
-            objCarregado.scale.z = scale;
-
-            objCarregado.position.z = 0;
-            objCarregado.position.x = 0;
-            objCarregado.position.y = -2;
-            objCarregado.rotation.x = rotationX;
-            objCarregado.rotation.z = rotationY;
-            objCarregado.rotation.y = rotationZ;
-            scene.add(objCarregado)
-        }
-    );
+var loadObj = function(){
+	objLoader = new THREE.OBJLoader();
+	objLoader.load('assets/cavalo.obj', function(object) {
+		cavalo = object;
+		object.traverse(function (child) {
+			if (child instanceof THREE.Mesh) {
+				child.material.color.setHex("0x003484");
+			}
+		});
+		cavalo.position.z = 20;
+		cavalo.position.x = 20;
+        cavalo.position.y = -2;
+        cavalo.rotation.x = 4.7;
+        cavalo.scale.z = 0.2;
+        cavalo.scale.x = 0.2;
+        cavalo.scale.y = 0.2;
+		cavalo.castShadow = true;
+		scene.add(cavalo);    
+	}, function(andamento) {
+		console.log((andamento.loaded / andamento.total *100) + "% pronto!");
+	}, function (error) { console.log(error); });
+	objLoader.load('assets/pato.obj', function(object) {
+		pato = object;
+		object.traverse(function (child) {
+			if (child instanceof THREE.Mesh) {
+				child.material.color.setHex("0xde4c84");
+			}
+		});
+		pato.position.z = 10;
+		pato.position.x = 20;
+        pato.position.y = -2;
+        pato.scale.x = 0.09;
+        pato.scale.y = 0.09;
+        pato.scale.z = 0.09;
+        pato.rotation.x = 4.6;
+		pato.castShadow = true;
+		scene.add(pato);    
+	}, function(andamento) {
+		console.log((andamento.loaded / andamento.total *100) + "% pronto!");
+	}, function (error) { console.log(error); });
+	objLoader.load('assets/elefante.obj', function(object) {
+		elefante = object;
+		object.traverse(function (child) {
+			if (child instanceof THREE.Mesh) {
+				child.material.color.setHex("0x4cc01e");
+			}
+		});
+		elefante.position.z = 10;
+		elefante.position.x = -150;
+		elefante.position.y = 0;
+		elefante.castShadow = true;
+		scene.add(elefante);    
+	}, function(andamento) {
+		console.log((andamento.loaded / andamento.total *100) + "% pronto!");
+	}, function (error) { console.log(error); });
+	objLoader.load('assets/porco.obj', function(object) {
+		porco = object;
+		object.traverse(function (child) {
+			if (child instanceof THREE.Mesh) {
+				child.material.color.setHex("0xba812d");
+			}
+		});
+		porco.position.z = 10;
+		porco.position.x = -80;
+        porco.position.y = 5;
+        porco.scale.y = 30;
+        porco.scale.x = 30;
+        porco.scale.z = 30;
+        porco.rotation.x = 4.7;
+        porco.rotation.y = 25.3;
+		porco.castShadow = true;
+		scene.add(porco);    
+	}, function(andamento) {
+		console.log((andamento.loaded / andamento.total *100) + "% pronto!");
+	}, function (error) { console.log(error); });
+	objLoader.load('assets/pinguin.obj', function(object) {
+		pinguin = object;
+		object.traverse(function (child) {
+			if (child instanceof THREE.Mesh) {
+				child.material.color.setHex("0x69456a");
+			}
+		});
+		pinguin.position.z = -20;
+		pinguin.position.x = -20;
+		pinguin.position.y = 20;
+		pinguin.castShadow = true;
+		scene.add(pinguin);    
+	}, function(andamento) {
+		console.log((andamento.loaded / andamento.total *100) + "% pronto!");
+	}, function (error) { console.log(error); });
 }
 
 var init = function() {
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0xcce0ff);
+	camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 140);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-    scene = new THREE.Scene();
+	loadObj();
 
-    camera = new THREE.PerspectiveCamera( 
+	spotLight = new THREE.SpotLight(0xffffff);
+	scene.add(spotLight);
+	spotLight.position.set(100, 100, 100);
+	spotLight.castShadow = true;
+	spotLight.shadow.mapSize.width = 100;
+	spotLight.shadow.mapSize.height = 100;
+	spotLight.shadow.camera.near = 1;
+	spotLight.shadow.camera.far = 99;
+	spotLight.shadow.camera.fov = 40;
 
-                                60                                          // angulo
-                                ,window.innerWidth / window.innerHeight     //aspect
-                                ,0.1                                       // Near
-                                ,1000                                      // Far
-                            );
+	renderer.shadowMap.enable = true;
+	renderer.shadowMap.type = THREE.BasicShadowMap;
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+	scene.add(new THREE.AmbientLight(0xffffff));
 
-    this.criaGround();
-    this.loadObj("cavalo", 0.2, 4.5, 4.5, 0);
-    this.loadObj("porco", 10, 4.5, 4.5, 0);
-    this.loadObj("pato", 0.09, 4.5, 4.5, 0);
-    this.loadObj("elefante", 0.09, 4.5, 4.5, 0);
-    this.loadObj("penguin", 10, 4.5, 4.5, 5);
-
-    //Iluminação
-    spotLight = new THREE.SpotLight( 0xffffff);
-    scene.add(spotLight);
-    spotLight.position.set(100,100,100);
-    spotLight.castShadow = true;
-    spotLight.shadow.mapSize.width = 2000;
-    spotLight.shadow.mapSize.height = 2000;
-    spotLight.shadow.camera.near = 1;
-    spotLight.shadow.camera.far = 99;
-    spotLight.shadow.camera.fov = 40;
-
-   
-	camera.position.set( 0, 20, 100 );
-
-    
-
-    //Essas linhas criam o gridView, lembrando que ele basicamente Ã© sÃ³ uma grade de linhas no eixo X
-    //scene.add( new THREE.GridHelper( 400, 40 ) );
-  
-    render();
-
-    document.addEventListener('keydown', onKeyDown );  
+	criaGround();
+	guiFunction();
+	render();
 };
 
-var ci = 0
 var render = function() {
-    requestAnimationFrame( render );
-
-    renderer.render( scene, camera );
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
 };
-
-var onKeyDown = function(e){
-    if(e.keyCode == 37){
-       camera.position.x+=-0.1;
-    }
-    if(e.keyCode == 39){
-        camera.position.x+=+0.1;
-    }
-    if(e.keyCode == 38){
-        camera.position.y+=+0.1;
-    }
-    if(e.keyCode == 40){
-        camera.position.y+=-0.1;
-    }
-    if(e.keyCode == 81){
-        camera.position.z+=-0.1;
-    }
-    if(e.keyCode == 65){
-        camera.position.z+=+0.1;
-    }
-    if(e.keyCode == 32){
-        camera.rotation.y+=+0.1;
-    }
-}
 
 window.onload = this.init;
+
+function toRadians(angle) {
+	return angle * (Math.PI / 180);
+}
